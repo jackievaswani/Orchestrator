4cb576d5f009d0c73eecf3b6b48702f6424ed2dd
Jackie <jackievaswani6@gmail.com>
2019-11-17 15:15:53 +0530

    Code Refactoring

--- src/main/java/vjti/orchestrator/App.java
+++ src/main/java/vjti/orchestrator/App.java
@@ -1,35 +1,40 @@
 package vjti.orchestrator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.json.simple.JSONObject;
 import vjti.orchestrator.model.Request;
 import vjti.orchestrator.service.Executor;
-import vjti.orchestrator.service.RestClientService;
+import vjti.orchestrator.builder.RestClientBuilder;
 
 public class App 
 {
     public static void main( String[] args )
     {
         Executor executor = new Executor();
-        RestClientService restClientService = new RestClientService(executor);
+        RestClientBuilder restClientBuilder = new RestClientBuilder(executor);
         
         List<Request> requests = new ArrayList<>();
         requests.add(new Request("https://jsonplaceholder.typicode.com/todos/1"));
         requests.add(new Request("https://jsonplaceholder.typicode.com/todos/2"));
         
-        restClientService
+        // use case: 
+        // Call 2 api in parallel & derive the input params for 3rd api from output of first 2 api 
+        // and finally call 3 api to get final result
+        
+        restClientBuilder
         .addRequests(requests)
         .then((List<JSONObject> result) -> {
         	System.out.println(result);
-        	requests.clear();
-        	requests.add(new Request("https://jsonplaceholder.typicode.com/todos/3"));	
-        	return requests;})
-        .then((List<JSONObject> result) -> {
+        	Integer param1=-1, param2=-1;
+        	if(result.get(0) != null && result.get(1) != null ) {
+        		param1=(Integer)result.get(0).get("id");
+        		param1=(Integer)result.get(1).get("id");
+        	}
+        	return Arrays.asList(new Request("https://jsonplaceholder.typicode.com/todos/3?param1="+param1 +"&param2="+param2));})
+        .execute((List<JSONObject> result) -> {
         	System.out.println(result);
-        	return null;
         });
-        
-        restClientService.execute();
     }
 }
--- src/main/java/vjti/orchestrator/builder/RestClientBuilder.java
+++ src/main/java/vjti/orchestrator/builder/RestClientBuilder.java
@@ -1,33 +1,37 @@
-package vjti.orchestrator.service;
+package vjti.orchestrator.builder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+
 import org.json.simple.JSONObject;
+
 import vjti.orchestrator.model.Request;
+import vjti.orchestrator.service.Executor;
 
-public class RestClientService {
-
+public class RestClientBuilder {
 	private Executor executor;
 	private List<Request> requests = new ArrayList<Request>();
 	private List<Function<List<JSONObject>,List<Request>>> callbacks = new ArrayList<>();
 	
-	public RestClientService(Executor executor) {
+	public RestClientBuilder(Executor executor) {
 		this.executor=executor;
 	}
 	
-	public RestClientService addRequests(List<Request> requests) {
+	public RestClientBuilder addRequests(List<Request> requests) {
 		this.requests.addAll(requests);
 		return this;
 	}
 	
-	public RestClientService then(Function<List<JSONObject>, List<Request>> function) {
+	public RestClientBuilder then(Function<List<JSONObject>, List<Request>> function) {
 		callbacks.add(function);
 		return this;
 	}
@@ -37,13 +41,12 @@ public class RestClientService {
 		requests.clear();
 	}
 	
-	public void execute(){
+	public void execute(Consumer<List<JSONObject>> consumerFn){
 		List<JSONObject> results = execute(requests);
-		//work around for assigning result variable in lambda
-		final List[] resultList = {results};
-		callbacks.forEach(fn -> {
-			resultList[0]=execute(fn.apply(resultList[0]));
-			});
+		for(Function<List<JSONObject>,List<Request> > fn : callbacks) {
+			results=execute(fn.apply(results));
+		}
+		consumerFn.accept(results);
 	}
 	
 	public List<JSONObject> execute(List<Request> apiRequest){
@@ -68,7 +71,4 @@ public class RestClientService {
 		};
 		return callable;
 	}
-	
-	
-	
 }
